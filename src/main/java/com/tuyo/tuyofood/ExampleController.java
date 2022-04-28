de8954999c3452f6122c3ed305ae54eeec7808fc
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-28 17:49:24 -0300

    #commit - 6 - Bean

--- src/main/java/com/tuyo/tuyofood/ExampleController.java
+++ src/main/java/com/tuyo/tuyofood/ExampleController.java
@@ -1,10 +1,11 @@
 package com.tuyo.tuyofood;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller                             /*informa que é uma classe responsável por receber requisições web*/
+                                        /*@Controller também transforma esta classe num Bean. Porque a classe Controller também recebe um @Component*/
+@Controller                             /*Controlador WEB: informa que é uma classe responsável por receber requisições web*/
 public class ExampleController {
 
     @GetMapping("/hello")               /*informa o caminho a percorrer*/
--- src/main/java/com/tuyo/tuyofood/notificacao/NotificadorEmail.java
+++ src/main/java/com/tuyo/tuyofood/notificacao/NotificadorEmail.java
@@ -4,7 +4,9 @@ import com.tuyo.tuyofood.modelo.Cliente;
 import org.springframework.stereotype.Component;
 
 /*Se eu quiser que o Spring gerencie essa classe, instancie e configure, e injete os objetos(beans) nela, é necessário "avisar" para ele:
-1. usando @Component(com isso estou dizendo que esta classe virou um componente Spring.*/
+1. usando @Component(com isso estou dizendo que esta classe virou um componente Spring.
+2. Componentes Spring são instanciados por ele.
+3. Com isso já temos um Bean Spring instanciado.*/
 
 @Component
 public class NotificadorEmail {
