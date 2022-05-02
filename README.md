fdae596e89eaa61ef9808967e2ecb6abcc9feb52
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-05-02 18:00:37 -0300

    #commit 1 - Injecao_de_Dependencia com Ambiguidade e @Primary

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Injeção de Dependência com Ambiguidade e Lista de Beans
+# Injeção de Dependência com Ambiguidade e @Primary
 
 ### É necessário evitar o erro de ambiguidade fazendo a desambiguação porque o Spring não sabe qual dos components é o principal.
 ### 
--- src/main/java/com/tuyo/tuyofood/service/ativacao/AtivacaoClienteService.java
+++ src/main/java/com/tuyo/tuyofood/service/ativacao/AtivacaoClienteService.java
@@ -6,20 +6,15 @@ import com.tuyo.tuyofood.notificacao.NotificadorEmail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
-/*Injetando uma lista de beans:*/
 @Component
 public class AtivacaoClienteService {
 
     @Autowired
-    private List<Notificador> notificadores;
+    private Notificador notificador;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        for (Notificador notificador : notificadores) {
             notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-        }
     }
 }
