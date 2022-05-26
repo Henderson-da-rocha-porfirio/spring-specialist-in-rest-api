168b3cec53672092cbc270cf971b17babef110c7
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-05-26 10:45:52 -0300

    #commit 2 - REST_HTTP_RESPONSE_ENTITY

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # HTTP - COM RESPONSEENTITY
-### Anotação: 
+### permite a customização da resposta HTTP
+### Verificar a KitchenController
+### Fazendo o teste no POstman:
+````
+File -> Settings -> Automatically follow redirects
+````
+### Verificar os Headers e verá a resposta. No console não aparecerá nada.
--- src/main/java/com/tuyo/tuyofood/api/controller/KitchenController.java
+++ src/main/java/com/tuyo/tuyofood/api/controller/KitchenController.java
@@ -4,11 +4,17 @@ import com.tuyo.tuyofood.api.model.KitchensXmlWrapper;
 import com.tuyo.tuyofood.domain.entity.Kitchen;
 import com.tuyo.tuyofood.domain.repository.KitchenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+/* ResponseEntity:
+ 1. permite a customização da resposta HTTP
+ 2. permite também a customização dos headers das respostas */
 
 @RestController
 @RequestMapping(value = "/kitchens")
@@ -17,7 +23,7 @@ public class KitchenController {
     @Autowired
     private KitchenRepository kitchenRepository;
 
-    /* Listando Coleções de Cozinha */
+    /* Listando Coleções de Kitchen */
     @GetMapping
     public List<Kitchen> listar() {
         return kitchenRepository.listar();
@@ -29,7 +35,22 @@ public class KitchenController {
     }
 
     @GetMapping("/{kitchenId}")
-    public Kitchen buscar(@PathVariable("kitchenId") Long kitchenId) {
-        return kitchenRepository.buscar(kitchenId);
+    public ResponseEntity<Kitchen> buscar(@PathVariable Long kitchenId) {
+        Kitchen kitchen = kitchenRepository.buscar(kitchenId);
+
+//      Definindo o Status:
+//		return ResponseEntity.status(HttpStatus.OK).body(kitchen);
+
+//      mesmo que o de cima mas é um atalho:
+//		return ResponseEntity.ok(kitchen);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.LOCATION, "http://localhost:8080/kitchens/");
+
+//      Customizando os
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .headers(headers)
+                .build();
     }
 }
