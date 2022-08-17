6d3edbd8f43fd46d38201d7fb9611e9fe6234dd9
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-08-17 15:36:55 -0300

    #Commit 2 EXCECOES_@ResponseStatus_Simplificando_Codigo

--- src/main/java/com/tuyo/tuyofood/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/tuyo/tuyofood/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,7 +1,7 @@
 package com.tuyo.tuyofood.domain.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /* 1. @ResponseStatus: retorno de status
  *  2. reason: mensagem customizada se quiser.
@@ -9,17 +9,13 @@ import org.springframework.web.server.ResponseStatusException;
  *  4. ResponseStatusException: esta classe anula a necessidade de usar @ResponseStatus aqui.
  *  5. Vantagem é ter uma única classe exception com vários status http*/
 
-// @ResponseStatus(value = HttpStatus.NOT_FOUND) //, reason = "Entidade não encontrada")
-public class EntidadeNaoEncontradaException extends ResponseStatusException {
+@ResponseStatus(value = HttpStatus.NOT_FOUND) //, reason = "Entidade não encontrada")
+public class EntidadeNaoEncontradaException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
-    public EntidadeNaoEncontradaException(HttpStatus status, String reason) {
-        super(status, reason);
-    }
-
     public EntidadeNaoEncontradaException(String reason) {
-        this(HttpStatus.NOT_FOUND, reason);
+        super(reason);
     }
 
 }
